0a33439ab8e8d02915b450fe2877a79e5e580ff1
ferchuse <sistemas@glifo.mx>
2020-08-30 23:45:41 -0500

    Update cnx_db.php

--- subs/cnx_db.php
+++ subs/cnx_db.php
@@ -1,19 +1,19 @@
 <?php
 
 //Conexion con la base
-if (!$MySQL=@mysql_connect('localhost', 'road', 'TKU2m3JkNpYJefBP')) {
+if (!$MySQL=@mysql_connect('localhost', 'syncsis_zitlalte', 'Zitlal@2020')) {
 	$t=time();
 	while (time()<$t+5) {}
-	if (!$MySQL=@mysql_connect('localhost', 'road', 'TKU2m3JkNpYJefBP')) {
+	if (!$MySQL=@mysql_connect('localhost', 'syncsis_zitlalte', 'Zitlal@2020')) {
 		$t=time();
 		while (time()<$t+10) {}
-		if (!$MySQL=@mysql_connect('localhost', 'road', 'TKU2m3JkNpYJefBP')) {
+		if (!$MySQL=@mysql_connect('localhost', 'road', 'Zitlal@2020')) {
 		echo '<br><br><br><h3 align=center">Hay problemas de comunicaci&oacute;n con la Base de datos.</h3>';
 		echo '<h4>Por favor intente mas tarde.-</h4>';
 		exit;
 		}
 	}
 }
-$base = "road";
+$base = "syncsis_zitlaltepec";
 mysql_select_db($base);
 ?>
