a983bb9fdcd4f4e45dfce4f7a3884129010832d8
ferchuse <sistemas@glifo.mx>
2020-10-02 17:39:57 -0500

    Update cnx_db.php

--- subs/cnx_db.php
+++ subs/cnx_db.php
@@ -20,6 +20,25 @@
 			}
 		}
 	}
+	
+	if($_SERVER["SERVER_NAME"] == "pruebas.grupozitlaltepec.com.mx" ){
+		
+		
+		$base = "rhgaazco_zitlaltepec";
+		if (!$MySQL=@mysql_connect('localhost', 'rhgaazco_zitlalte', 'Zitla@2020')) {
+			$t=time();
+			while (time()<$t+5) {}
+			if (!$MySQL=@mysql_connect('localhost', 'rhgaazco_zitlalte', 'Zitla@2020')) {
+				$t=time();
+				while (time()<$t+10) {}
+				if (!$MySQL=@mysql_connect('localhost', 'rhgaazco_zitlalte', 'Zitla@2020')) {
+					echo '<br><br><br><h3 align=center">Hay problemas de comunicaci&oacute;n con la Base de datos.</h3>';
+					echo '<h4>Por favor intente mas tarde.-</h4>';
+					exit;
+				}
+			}
+		}
+	}
 	else{
 		$base = "rhgaazco_zitlaltepec";
 		if (!$MySQL=@mysql_connect('localhost', 'rhgaazco_zitlalte', 'Zitla@2020')) {
