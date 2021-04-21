bfc584ed7a95c968972b5eee5956e952c9d5b98b
ferchuse <sistemas@glifo.mx>
2021-04-21 13:00:32 -0500

    Create descargar_tablas.php

---
+++ bases/consultas/descargar_tablas.php
@@ -0,0 +1,43 @@
+<?php 
+	// session_start();
+	include('../../conexi.php');
+	$link = Conectarse();
+	
+	$folio_tarjeta = substr ($_GET["barcode"], 6,6);
+	
+	$respuesta["folio_tarjeta"] = $folio_tarjeta;
+	
+	
+	$consulta_checadores = "		
+	SELECT 
+	*
+	FROM checadores 
+	LEFT JOIN bases USING(id_base)
+	
+	
+	";
+	
+	$result_checadores = mysqli_query($link,$consulta_checadores);
+	
+	
+	
+	
+	if($result_checadores){
+		
+		while($row = mysqli_fetch_assoc($result_checadores)){
+			
+			$checadores[] = $row;
+		}
+		
+		$respuesta["checadores"] = $checadores;
+	}
+	else {
+			$respuesta["estatus"] = "error";
+			$respuesta["mensaje"] = "Error en ".$consulta_checadores.mysqli_Error($link);
+		}
+	
+	
+	echo json_encode($respuesta);
+	
+	
+?>							
