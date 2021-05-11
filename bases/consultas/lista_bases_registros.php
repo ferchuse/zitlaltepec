86814f00f5b1f0a65fcb01cdb59ce49bbb7d6447
ferchuse <sistemas@glifo.mx>
2021-05-11 12:27:55 -0500

    Corrige Lista registros

--- bases/consultas/lista_bases_registros.php
+++ bases/consultas/lista_bases_registros.php
@@ -12,9 +12,9 @@
 	
 	$consulta = "SELECT *
 	FROM bases_registros
-	
-	LEFT JOIN monitoreo ON monitoreo.tarjeta = recaudacion_monitoreo.tarjeta
-	
+	LEFT JOIN checadores USING(id_checadores)
+	LEFT JOIN bases ON bases_registros.id_base = bases.id_base
+
 	WHERE 1
 	";
 	
@@ -29,7 +29,7 @@
 		$consulta.=  " AND usuarios.cve = '{$_GET["usuarios_cve"]}'"; 
 	}
 	
-	$consulta.=  " ORDER BY recaudacion_monitoreo.cve"; 
+	// $consulta.=  " ORDER BY recaudacion_monitoreo.cve"; 
 	
 	
 	
@@ -53,86 +53,35 @@
 	<table class="table table-bordered table-condensed" id="dataTable" width="100%" cellspacing="0">
 		<thead>
 			<tr>
-				<th></th>
-				<th>Folio</th>
+				<th>Folio Tarjeta</th>
 				<th>Fecha </th>
-				<th>Recaudación </th>
 				<th>Unidad</th>
-				<th>Operador</th>
-				<th>Tarjeta</th>
-				<th>Empresa</th>
-				<th>Observaciones</th>
-				<th>Efectivo Recaudado</th>
-				<th>Efectivo a Entregar</th>
+				<th>Base</th>
 				<th>Usuario</th>
 			</thead>
 			<tbody id="tabla_DB">
 				<?php 
 					foreach($filas as $index=>$fila){
 					?>
-					<tr>
-						<td class="text-center"> 
-							<?php if($fila["recaudacion_estatus"] != 'C'){
-								
-								$totales[0]+= $fila["efectivo_recaudado"];
-								$totales[1]+= $fila["efectivo_entregar"];
-								if(dame_permiso("recaudacion.php", $link) == '3'){ //Permiso Supervisor
-									// echo dame_permiso("recaudacion.php", $link);
-								?>
-								<button class="btn btn-danger cancelar" title="Cancelar" data-id_registro='<?php echo $fila['recaudacion_monitoreo_cve']?>'>
-									<i class="fas fa-times"></i>
-								</button>
-								<button class="btn btn-outline-info imprimir" data-id_registro='<?php echo $fila['recaudacion_monitoreo_cve']?>'>
-									<i class="fas fa-print"></i>
-								</button>
-								<?php
-								}
-							?>
-							
-							<?php
-							}
-							else{
-								echo "<span class='badge badge-danger'>CANCELADO<br>".$fila["recaudacion_fechacan"]."<br>".$fila["usuario_cancela"]."</span>";
-							}
-							?>
-						</td>
-						<td><?php echo $fila["recaudacion_monitoreo_cve"]?></td>
-						<td><?php echo $fila["fecha"]?></td>
-						<td><?php echo $fila["recaudacion_nombre"]?></td>
-						<td><?php echo $fila["no_eco"]?></td>
-						<td><?php echo $fila["operadores_nombre"]?></td>
+					<tr>						
 						<td><?php echo $fila["tarjeta"]?></td>
-						<td><?php echo $fila["empresas_nombre"]?></td>
-						<td><?php echo $fila["obs"]?></td>
-						<td>$<?php echo $fila["efectivo_recaudado"]?></td>
-						<td>$<?php echo $fila["efectivo_entregar"]?></td>
-						<td><?php echo $fila["usuarios_nombre"]?></td>
-						
+						<td><?php echo $fila["fecha_registro"]?></td>
+						<td><?php echo $fila["num_eco"]?></td>
+						<td><?php echo $fila["base"]?></td>
+						<td><?php echo $fila["nombre"]?></td>
 					</tr>
 					<?php
 					}
 				?>
 			</tbody>
-			<tfoot>
+			<tfoot class="bg-secondary h5 text-white">
 				<tr>
+					<td><?php echo count($filas);?> Registros</td>
 					<td></td>
 					<td></td>
 					<td></td>
 					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<?php
-						foreach($totales as $i =>$total){
-						?>
-						<td class="h6">$<?php echo number_format($total)?></td>
-						<?php	
-						}
-					?>
-					<td></td>
-					<td></td>
+					
 				</tr>
 			</tfoot>
 		</table>
